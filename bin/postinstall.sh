d1bdbff921d1a282e67908183c7b04b7b7bfc2d2
Hannes Landstedt <github.com@zarac.se>
2013-07-20 13:34:27 +0200

    forgotten file (was ignored)

---
+++ bin/postinstall.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# enable some libraries on client side
+cp node_modules/dustjs-linkedin/dist/dust-full-1.2.0.js static/js/
+cp node_modules/dustjs-helpers/dist/dust-helpers-1.1.1.js static/js/
+cp node_modules/underscore/underscore-min.js static/js/
